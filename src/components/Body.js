4193ab20c867fed3a403a279cf3d8e35fe5cd35e
Istakhar Ahmad <istakhara21@gmail.com>
2025-09-29 17:18:45 +0530

    I have fetching all the menu data from swiggy and displaying

--- src/components/Body.js
+++ src/components/Body.js
@@ -29,7 +29,7 @@ const Body = () => {
       "https://www.swiggy.com/dapi/restaurants/list/v5?lat=21.99740&lng=79.00110&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
     );
     const json = await data.json();
-    console.log(json);
+    // console.log(json);
     setAllRestaurants(
       json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants
     );
--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -1,28 +1,59 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import ShimmerUI from "./ShimmerUI";
+import { useParams } from "react-router";
 
 const RestaurantMenu = () => {
+  //creating a state variable for updating Res info.
+  const [resInfo, setResInfo] = useState();
 
-    useEffect(()=> {
-        FetchMenu();
-    }, []);
+  const params = useParams();
+  console.log(params);
 
-    const FetchMenu = async () => {
-        const data = await fetch("https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.5399515&lng=77.25915789999999&restaurantId=1022629&catalog_qa=undefined&submitAction=ENTER");
-        const json = await data.json();
+  useEffect(() => {
+    FetchMenu();
+  }, []);
 
-        console.log(json);
-    }
-    return(
-        <div className="menu">
-            <h1>Name of the Restaurant</h1>
-            <h2>Menu</h2>
-            <ul>
-                <li>Biryani</li>
-                <li>Burgers</li>
-                <li>Soft Drinks</li>
-            </ul>
-        </div>
-    )
-}
+  const FetchMenu = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.5399515&lng=77.25915789999999&restaurantId=655878&catalog_qa=undefined&submitAction=ENTER"
+    );
+    const json = await data.json();
+
+    // console.log(json);
+    setResInfo(json.data);
+  };
+
+  if (!resInfo) {
+    return <ShimmerUI />;
+  }
+
+  const info = resInfo?.cards?.[2]?.card?.card?.info;
+  if (!info) {
+    return <ShimmerUI />;
+  }
+
+  const { name, areaName, avgRating, costForTwoMessage, locality } = info;
+
+  const itemCards =
+    resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card?.card
+      ?.itemCards;
+//   console.log(itemCards);
+  return (
+    <div className="menu">
+      <h1>{name}</h1>
+      <p>{areaName}</p>
+      <p>{avgRating}</p>
+      <p>{costForTwoMessage}</p>
+      <p>{locality}</p>
+      <ul>
+        {itemCards.map((item) => (
+          <li key={item?.card?.info?.id}>
+            {item?.card?.info?.name} - ₹{item?.card?.info?.price/100}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default RestaurantMenu;
