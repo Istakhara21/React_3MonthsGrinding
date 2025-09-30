d3e60e6f25f99bf0022da20e8f8466a46e4cb750
Istakhar Ahmad <istakhara21@gmail.com>
2025-09-30 16:27:39 +0530

    learned api call in class based components

--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -6,7 +6,10 @@ class UserClass extends React.Component {
     // console.log(this.props.name + "Child Constructor");
 
     this.state = {
-      count: 0,
+      userInfo: {
+        name: "Dummy",
+        location: "Default",
+      },
     };
   }
   async componentDidMount() {
@@ -16,25 +19,17 @@ class UserClass extends React.Component {
     const json = await data.json();
 
     console.log(json);
+
+    this.setState({
+      userInfo: json,
+    });
   }
   render() {
-    const { name } = this.props;
-    const { count } = this.state;
-    // console.log(this.props.name + "Child Render");
+    const {name, location} = this.state.userInfo;
     return (
       <div className="user-about">
-        <h1>
-          Count: {count}{" "}
-          <button
-            onClick={() => {
-              this.setState({ count: this.state.count + 1 });
-            }}
-          >
-            Count Inc
-          </button>
-        </h1>
-
         <h1>Name: {name}</h1>
+        <h2>Location: {location}</h2>
         <h2>UserName: Istakhara21</h2>
         <h4>Contact: @Helloworld</h4>
       </div>
